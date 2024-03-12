9b3b7427c95bfd700ef339860af4ea14855d1ef8
rwaming <art.rwam@gmail.com>
2024-03-13 02:05:54 +0900

    7569: Split text to m, n, h, tomatoes by m
    Next time, start by splitting 'tomatoBoxM' and put them in 'box' array.

--- 7569/7569.js
+++ 7569/7569.js
@@ -1,7 +1,15 @@
-function solution() {}
+function solution(mnh, ...textArr) {
+  const [m, n, h] = mnh.split(/\s/);
+  const box = [];
 
-const filePath = 'input.txt';
+  // put tomato in box
+  const tomatoBoxM = textArr.map((text) => text.split(/\s/));
+  tomatoBoxM.forEach((v, i) => {});
+}
+
+const filePath = '/Users/rwam/project/test/7569/input.txt';
 // const filePath = "/dev/stdin";
-const input = require('fs').readFileSync(filePath, 'utf8');
+const file = require('fs').readFileSync(filePath, 'utf8');
+const input = file.split('\n');
 
-solution(input);
+solution(...input);
